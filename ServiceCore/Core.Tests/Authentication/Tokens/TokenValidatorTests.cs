dbe2416e6fa323f4193f330838081a80621ae952
Niels Boerkamp <n.boerkamp@outlook.com>
2018-06-11 18:50:07 +0200

    Added TokenValidatorTest file

---
+++ ServiceCore/Core.Tests/Authentication/Tokens/TokenValidatorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Authentication.Tokens
+{
+    [TestClass]
+    public class TokenValidatorTests
+    {
+    }
+}
