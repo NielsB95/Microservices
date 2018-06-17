aa1a288d60803a6d5d89087a92a997e85301a7ed
Niels Boerkamp <n.boerkamp@outlook.com>
2018-06-17 19:42:51 +0200

    Renamed settings file

--- MarkdownService/Markdown.GUI/src/components/Home.vue
+++ MarkdownService/Markdown.GUI/src/components/Home.vue
@@ -4,10 +4,10 @@
 		<b-table :items=documents :fields="documentFields">
 			<template slot="actions" slot-scope="data">
 				<div class="float-right">
-					<b-btn variant="link" class="pointer">
+					<b-btn variant="light" class="pointer">
 						<a href=""><span class="fa fa-edit"></span></a>
 					</b-btn>
-					<b-btn variant="link" class="pointer">
+					<b-btn variant="light" class="pointer">
 						<router-link :to="{ name: 'ReadDocument', params: { docid: data.item.id }}"><span class="fa fa-eye"></span></router-link>
 					</b-btn>
 				</div>
--- MarkdownService/Markdown.GUI/src/components/blocks/navigation.vue
+++ MarkdownService/Markdown.GUI/src/components/blocks/navigation.vue
@@ -1,31 +1,37 @@
 <template>
-    <b-navbar toggleable="md" type="dark" variant="primary">
-    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-    <b-navbar-brand href="#/">Markdown</b-navbar-brand>
+    <div class="header">
+        <b-navbar toggleable="md" type="dark" variant="primary">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#/">Markdown</b-navbar-brand>
 
-    <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
-        <b-nav-item href="#/">Home</b-nav-item>
-        </b-navbar-nav>
+        <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav>
+            <b-nav-item href="#/">Home</b-nav-item>
+            </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
-            <template slot="button-content">
-            <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Signout</b-dropdown-item>
-        </b-nav-item-dropdown>
-        </b-navbar-nav>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+                <!-- Using button-content slot -->
+                <template slot="button-content">
+                <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Signout</b-dropdown-item>
+            </b-nav-item-dropdown>
+            </b-navbar-nav>
 
-    </b-collapse>
-    </b-navbar>
+        </b-collapse>
+        </b-navbar>
+    </div>
 </template>
-
 <script>
 export default {
   name: "Navigation"
 };
 </script>
+<style scoped>
+.header {
+  padding-bottom: 2em;
+}
+</style>
